4548fb10b37c5c218aa3b9901436e64f1fde6810
kalinduSsasinindu <kalindusasinindu153@gmail.com>
2025-06-14 22:50:34 +0530

    ene

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:notehider/main.dart';
 
-void main() {
+void main() {////dsfdfd
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
